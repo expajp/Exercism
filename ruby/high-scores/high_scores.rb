725e3b5be7475070fec0d6bd32cb3e04fa06b425
Shu OGAWARA <expajp@gmail.com>
2019-09-30 22:05:29 +0900

    use the method generated by attribute_reader and max(attr)

--- ruby/high-scores/high_scores.rb
+++ ruby/high-scores/high_scores.rb
@@ -6,24 +6,18 @@ class HighScores
   end
   
   def latest
-    @scores.last
+    scores.last
   end
 
   def personal_best
-    descending_scores.first
+    scores.max
   end
 
   def personal_top_three
-    descending_scores.take(3)
+    scores.max(3)
   end
   
   def latest_is_personal_best?
     latest == personal_best
-  end
-
-  private
-
-  def descending_scores
-    @scores.sort { |a, b| b <=> a }
   end
 end
